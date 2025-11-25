db0f63ba1ba9a943772092e933ae5459b90d20e8
khushil <choprakhushil13@gmail.com>
2025-11-25 16:41:40 +0530

    fontend completed

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+// const express = require("express");
+// const cors = require("cors");
+
+// const app = express();
+
+// // Middlewares
+// app.use(cors());
+// app.use(express.json());
+
+// // Test route
+// app.get("/", (req, res) => {
+//   res.send("Backend is running!");
+// });
+
+// // Tasks route (sample)
+// app.get("/tasks", (req, res) => {
+//   res.json([
+//     { id: 1, task: "Learn Docker" },
+//     { id: 2, task: "Build To-Do App" }
+//   ]);
+// });
+
+// // Start server
+// app.listen(5000, () => {
+//   console.log("Server running on port 5000");
+// });
